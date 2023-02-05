faf9630c706e6b5bb32fce705a299a6f540f0bf3
Kane <95705546+kaneran@users.noreply.github.com>
2023-02-05 12:06:17 +0000

    Save new revision of document + test

--- services/documentService.js
+++ services/documentService.js
@@ -21,20 +21,37 @@ function getAvailableRevisions(documentTitle) {
   if (availableRevisions.length > 0) {
     return availableRevisions;
   } else {
-    throw "Document does not exist.";
+    throw "Document does not exist or no revisions.";
   }
 }
 
-function getLatestDocument() {
-  const availableRevisions = getAvailableRevisions("Title 1");
+function getLatestDocument(documentTitle) {
+  const availableRevisions = getAvailableRevisions(documentTitle);
   const sortedAvailableRevisions = availableRevisions.sort(
     (a, b) => b.version - a.version
   );
   return sortedAvailableRevisions[0];
 }
 
+function addNewDocumentRevision(documentTitle, content) {
+  const allRevisions = getAllDocumentVersions();
+  const { version } = getLatestDocument(documentTitle);
+  //In reality, I would insert this into the database
+  const newRevisions = [
+    ...allRevisions,
+    {
+      title: documentTitle,
+      version: version + 1,
+      content,
+      timestamp: Date(Date.now()).toString(),
+    },
+  ];
+  return newRevisions;
+}
+
 module.exports = {
   getAvailableTitles,
   getAvailableRevisions,
   getLatestDocument,
+  addNewDocumentRevision,
 };
--- tests/documentService.test.js
+++ tests/documentService.test.js
@@ -1,8 +1,10 @@
-const { getAvailableTitles, getAvailableRevisions, getLatestDocument} = require("../services/documentService");
+const { getAllDocumentVersions } = require("../repositories/documentRepository");
+const { getAvailableTitles, getAvailableRevisions, getLatestDocument, addNewDocumentRevision} = require("../services/documentService");
 
 test('Get list of available titles', () => {
     const availableTitles = getAvailableTitles();
     const firstAvailableTitle = availableTitles[0];
+
     expect(availableTitles.length).toBeGreaterThan(0);
     expect(firstAvailableTitle).toBe("Title 1");
 });
@@ -10,18 +12,31 @@ test('Get list of available titles', () => {
 test('Get list of available revisions for a document' , () => {
     const availableRevisions = getAvailableRevisions("Title 1");
     const {version, content, timestamp} = availableRevisions[0];
+
     expect(availableRevisions.length).toBeGreaterThan(0);
     expect(version).toBe(1);
     expect(content).toBe("Hello");
     expect(timestamp).toBe("1pm")
 });
 
-test('Return empty array if there are no revisions for a document', () => {
-    expect(() => getAvailableRevisions("Invalid title")).toThrow(/^Document does not exist.$/);
+test('Throw error if there are no revisions for a document', () => {
+    expect(() => getAvailableRevisions("Invalid title")).toThrow(/^Document does not exist or no revisions.$/);
 });
 
 
 test('Get the current latest version of the document', () => {
     const {version} = getLatestDocument("Title 1");
     expect(version).toBe(4);
+});
+
+test('Save new revision of a document',() => {
+    const [title, documentContent] = ["Title 1", "This is soo cool!"];
+    const allDocumentVersions = getAllDocumentVersions();
+    const result = addNewDocumentRevision(title, documentContent);
+    const {version} = getLatestDocument(title);
+    const latestDocumentRevision = result[result.length - 1];
+
+    expect(result.length).toBe(allDocumentVersions.length + 1);
+    expect(latestDocumentRevision.content).toBe(documentContent);
+    expect(latestDocumentRevision.version).toBe(version + 1);
 });
