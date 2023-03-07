627cf8573dd5d96ab413e965101cb884651ce98d
Kane <95705546+kaneran@users.noreply.github.com>
2023-03-07 21:44:29 +0000

    Resolve typo

--- services/validationService.js
+++ services/validationService.js
@@ -1,6 +1,6 @@
 function validateDocumentTitle(documentTitle) {
   if (documentTitle.length > 50) {
-    throw "Title must have maximum length of 50 characters!!!"
+    throw "Title must have maximum length of 50 characters"
   }
 }
 
